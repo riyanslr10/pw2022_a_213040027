6252b3148f6763473eec9d7777b6030429efbc44
riyanslr10 <riyan.putras40@gmail.com>
2022-06-11 03:07:44 +0700

    update ajax

--- tubes/ajax/baranguser.php
+++ tubes/ajax/baranguser.php
@@ -1,19 +1,20 @@
 <?php 
 require '../php/functions.php';
 $keyword = $_GET['keyword'];
-$query = "SELECT * FROM produkstik
+$query = "SELECT * FROM produk
             WHERE
-            nama_stik LIKE '%$keyword%'
+            nama_produk LIKE '%$keyword%'
             ";
 $produk = query($query);
  ?>
  <table border="1" cellpadding="13" cellspacing="0">
         <tr class="color1">
             <th>No</th>
-            <th>Option</th>
             <th>Image</th>
             <th>Name product</th>
             <th>description product</th>
+            <th>Price</th>
+            <th>Stock</th>
         </tr>
         <?php if(empty($produk)) : ?>
             <tr>
@@ -26,13 +27,11 @@ $produk = query($query);
         <?php foreach($produk as $p) : ?>
             <tr>
                 <td><?= $i; ?></td>
-                <td>
-                    <a href="ubah.php?id=<?= $p['id_stik'] ?>"><button class="color2">Edit</button></a>
-                    <a href="hapus.php?id=<?= $p['id_stik'] ?>" onclick="return confirm('Hapus Data??')"><button class="color3">Delete</button></a>
-                </td>
                 <td><img style="width: 500px;" src ='../assets/img/<?= $p['img']; ?>'></td>
-                <td><?= $p['nama_stik']; ?></td>
-                <td><?= $p['deskripsi_stik']; ?></td>
+                <td><?= $p['nama_produk']; ?></td>
+                <td><?= $p['deskripsi_produk']; ?></td>
+                <td><?= $p['Harga']; ?></td>
+                <td><?= $p['stok']; ?></td>
             </tr>
             <?php $i++; ?>
         <?php endforeach; ?>
